63ca8218536cb296be5471d38270be1d8baeccfe
fukada-m <fukada.m8888@gmail.com>
2023-06-16 18:46:50 +0900

    記事を表示する機能

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,21 +5,34 @@ class ArticlesController < ApplicationController
         @article = Article.new(@body)
         @article.slug = create_slug(@article.title)
         if @article.save 
-            render json: {article: {
-                slug: @article.slug,
-                title:  @article.title,
-                description: @article.description,
-                body: @article.body,
-                createdAt: @article.created_at,
-                updatedAt: @article.updated_at
-                }
-            }, status: :ok
+            respose_article(@article)
         else
             render json: { errors: @article.errors.full_messages }, status: :unprocessable_entity
         end
     end
 
+    def show
+        @article = Article.find_by(slug: params[:slug])
+        if @article
+            respose_article(@article)
+        else
+            render json: { errors: "該当する記事はありませんでした" }, status: :unprocessable_entity
+        end
+    end
+
     private
+
+    def respose_article(article)
+        render json: {article: {
+            slug: article.slug,
+            title:  article.title,
+            description: article.description,
+            body: article.body,
+            createdAt: article.created_at,
+            updatedAt: article.updated_at
+            }
+        }, status: :ok
+    end
 
     def create_slug(title)
         title.downcase.gsub(" ", "-")
--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -5,11 +5,31 @@ RSpec.describe ArticlesController, type: :controller do
         @controller = ArticlesController.new
         @article = FactoryBot.create(:article)
     end
+    
+    describe "create_slug" do
 
-    it 'slugはtitleを小文字にしてスペースをハイフンに変える' do
-        slug = controller.send(:create_slug, @article.title)
-        expected_slug = "how-to-train-your-dragon"
+        it 'slugはtitleを小文字にしてスペースをハイフンに変える' do
+            slug = controller.send(:create_slug, @article.title)
+            expected_slug = "how-to-train-your-dragon"
+            
+            expect(slug).to eq(expected_slug)
+        end
+    end
+
+    describe "GET #show" do
+        before do
+            @article.slug = controller.send(:create_slug, @article.title)
+            @article.save
+        end
+
+        it "当てはまる記事があった場合は正常にレスポンスを返す" do
+            get :show, params: { slug: "how-to-train-your-dragon" }
+            expect(response).to have_http_status(:success)
+        end
         
-        expect(slug).to eq(expected_slug)
+        it "当てはまる記事がなかった場合はエラーレスポンスを返す" do
+            get :show, params: { slug: "test-message" }
+            expect(response).to have_http_status(:unprocessable_entity)
+        end
     end
 end

984cda341449b98e52a1b4f36348bf8554aca404
fukada-m <fukada.m8888@gmail.com>
2023-06-19 08:54:32 +0900

    リファクタリング

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,7 @@ class ArticlesController < ApplicationController
         @article = Article.new(@body)
         @article.slug = create_slug(@article.title)
         if @article.save 
-            response_article(@article)
+            response_article
         else
             render json: { errors: @article.errors.full_messages }, status: :unprocessable_entity
         end
@@ -15,9 +15,9 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find_by(slug: params[:slug])
         if @article
-            response_article(@article)
+            response_article
         else
-            render json: { errors: "該当する記事はありませんでした" }, status: :unprocessable_entity
+            not_found_article
         end
     end
 
@@ -25,11 +25,10 @@ class ArticlesController < ApplicationController
         @article = Article.find_by(slug: params[:slug])
         @body = params.require(:article).permit(:title)
         if @article
-            update_article(@body["title"])
-            @article.save
-            render json: {article: @article}, status: :ok
+            update_article
+            response_article
         else
-            render json: { errors: "該当する記事はありませんでした" }, status: :unprocessable_entity
+            not_found_article
         end
     end
 
@@ -39,31 +38,27 @@ class ArticlesController < ApplicationController
             @article.destroy
             render json: {status: :ok}
         else
-            render json: {errors: "該当する記事はありませんでした。"}, status: :unprocessable_entity
+            not_found_article
         end
     end
 
     private
 
-    def update_article(title)
-        @article.title = title
-        @article.slug = create_slug(title)
+    def update_article
+        @article.slug = @body["title"]
+        @article.save
     end
 
-    def response_article(article)
-        render json: {article: {
-            slug: article.slug,
-            title:  article.title,
-            description: article.description,
-            body: article.body,
-            createdAt: article.created_at,
-            updatedAt: article.updated_at
-            }
-        }, status: :ok
+    def response_article
+        render json: {article: { article: @article}}, status: :ok
+    end
+
+    def not_found_article
+        render json: {errors: "該当する記事はありませんでした。"}, status: :unprocessable_entity
+        
     end
 
     def create_slug(title)
         title.downcase.gsub(" ", "-")
-
     end
 end

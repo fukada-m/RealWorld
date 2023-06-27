4b800569e20601c39f7b90e528732e667d8218e5
fukada-m <fukada.m8888@gmail.com>
2023-06-27 14:02:11 +0900

    helloyunonn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   def hello
-    render html: "<h1>Hello, Real World</h1>".html_safe
+    render html: "<h1>Hello, ゆのん</h1>".html_safe
   end
 end
 

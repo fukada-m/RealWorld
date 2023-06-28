19156327940de291db86a9af1630db45b9572228
fukada-m <fukada.m8888@gmail.com>
2023-06-28 12:31:08 +0900

    テストを通るようにした

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-  config.dsn = 
+#   config.dsn = 
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # Set traces_sample_rate to 1.0 to capture 100%
